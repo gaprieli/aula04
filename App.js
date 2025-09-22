1a6a74c6f2a98e75c2dd58935c9e83ccb84afb0e
gaprieli <gabrieliborbavalerio@gmail.com>
2025-09-22 10:49:02 -0300

    alterações

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, Image, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, ImageBackground, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
@@ -8,29 +8,49 @@ export default function App() {
       id: '1',
       nome: 'Aria',
       pelagem: 'Branca',
-      corolho: 'Marrom Cocô',
-      icone: require('./assets/aria.jpg')
+      corolho: 'Azul Cocô',
+      icone: require('./assets/aria.jpg'),
+      descricao: 'Aria é muito chata e insuportável'
     },
     {
       id: '2',
       nome: 'Spencer',
       pelagem: 'Branca',
       corolho: 'Marrom Bombom',
-      icone: require('./assets/spencer.jpg')
+      icone: require('./assets/spencer.jpg'),
+      descricao: 'Spencer é uma diva inteligente'
     },
     {
       id: '3',
       nome: 'Hanna',
       pelagem: 'Branca',
       corolho: 'Azul Gelo',
-      icone: require('./assets/hanna.jpg')
+      icone: require('./assets/hanna.jpg'),
+      descricao: 'Hanna é uma queridona meio burrinha'
     },
     {
       id: '4',
       nome: 'Emily',
       pelagem: 'Parda',
       corolho: 'Marrom Coco',
-      icone: require('./assets/emily.jpg')
+      icone: require('./assets/emily.jpg'),
+      descricao: 'Emily é a atleta do grupo, uma diva meio chatinha'
+    },
+    {
+      id: '5',
+      nome: 'Mona',
+      pelagem: 'Branca',
+      corolho: 'Marrom Mel',
+      icone: require('./assets/mona.jpg'),
+      descricao: 'Mona é a mais mais'
+    },
+    {
+      id: '6',
+      nome: 'Alison',
+      pelagem: 'Branca',
+      corolho: 'Azul Fumaça',
+      icone: require('./assets/alisson.jpg'),
+      descricao: 'Se acha demais'
     }
   ]
 
@@ -42,6 +62,7 @@ export default function App() {
       <FlatList
       data={lista}
       renderItem={({ item }) => (
+        <TouchableOpacity style={styles.viewItem} onPress={() => alert(item.descricao)}>
         <View style={styles.viewItem}>
           <Image style={styles.icone} source={item.icone} />
           <View>
@@ -50,6 +71,7 @@ export default function App() {
             <Text>Cor do Olho: {item.corolho}</Text>
           </View>
         </View>
+        </TouchableOpacity>
       )}
     />
     </ImageBackground>
